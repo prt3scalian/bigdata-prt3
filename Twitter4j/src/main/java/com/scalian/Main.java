0ee91cea2ce81b6e11fa0eb664e6eb5c3d999224
prt3 <prt3>
2018-09-18 18:18:16 +0200

    minor changes :
    add an output operation (DStream.print) to fix bug :
    
    java.lang.IllegalArgumentException: requirement failed: No output
    operations registered, so nothing to execute

--- Twitter4j/src/main/java/com/scalian/Main.java
+++ Twitter4j/src/main/java/com/scalian/Main.java
@@ -93,7 +93,7 @@ public class Main {
             	return sentiment;
             	});
             
-//            sentimentDStream.print();
+            sentimentDStream.print();
             
             System.out.println("SparkNamexTweet:start: starting Streaming computation by jssc.start()..........");
             jssc.start();
